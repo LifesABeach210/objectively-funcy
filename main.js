dfb1fef1a8d3a9e90b2b407b5556de54be7c8f1d
Dan <danielrothwell622@gmail.com>
2022-03-07 16:32:12 -0500

    hello

--- main.js
+++ main.js
@@ -1,4 +1,46 @@
 // Your code here.
+function getFirstName(box) {
+  let box2 = box;
+
+return box2.firstName;
+
+
+}
+function getLastName(box) {
+  let box2 = box;
+
+  return box2.lastName;
+}
+function getFullName(box) {
+  let box2 = box;
+
+return box2.firstName+" "+box2.lastName;
+
+
+}
+
+function setFirstName(person1,person2) {
+let a = person2.firstName + "Anthony";
+let b=person1.firstName + "Redacted";
+return a&&b ;
+}
+
+function setAge(person1,person2) {
+
+person1.age = person1.age + 6;
+person2.age = person2.age +6;
+
+return;
+
+}
+function giveBirthday(person1,person2) {
+person1.age++
+person2.age++;
+return;
+
+
+}
+
 
 
 
