7418a4148d4bdc89b02deac630c4c5c55de013e1
Dan <danielrothwell622@gmail.com>
2022-03-07 21:21:41 -0500

    hello

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 // Your code here.
 function getFirstName(box) {
-  let box2 = box;
+  
 
-return box2.firstName;
+return box.firstName;
 
 
 }
@@ -19,31 +19,49 @@ return box2.firstName+" "+box2.lastName;
 
 }
 
-function setFirstName(person1,person2) {
-let a = person2.firstName + "Anthony";
-let b=person1.firstName + "Redacted";
-return a&&b ;
+function setFirstName(person1,Str) {
+person1.firstName = Str;
+
 }
 
-function setAge(person1,person2) {
+function setAge(person1,newAge) {
 
-person1.age = person1.age + 6;
-person2.age = person2.age +6;
+person1.age= newAge; 
+
 
 return;
 
 }
-function giveBirthday(person1,person2) {
-person1.age++
-person2.age++;
+function giveBirthday(person1) {
+
+if (person1['age']===undefined) {
+  person1.age=1;
+}else{
+  person1.age++;
+}
+
+
 return;
 
 
 }
 
+function marry(person1,person2) {
+  person1.married = true;
+  person2.married = true;
+  person1.spouseName = getFullName(person2);
+  person2.spouseName = getFullName(person1);
 
 
+}
 
+function divorce(person1,person2) {
+  person1.married = false;
+  person2.married = false;
+  delete person1.spouseName;
+  delete person2.spouseName;
+
+}
 
 
 // Our code here. Don't touch!
